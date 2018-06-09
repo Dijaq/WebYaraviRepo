08db6814e2a75499dd74aa4218a9472d18d09653
djavierq <dijaq08@gmail.com>
2018-06-09 01:42:36 -0500

    Constantes

---
+++ config/constantes.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+	'estado_habilitado' => 2,
+	'estado_deshabilitado' => 1,
+	'estado_eliminado' => 0,
+	'prioridad_principal' => 1,
+	'prioridad_secundaria' => 2,
+	'prioridad_comun' => 3,
+	'numero_noticias_relacionadas' => 4
+);
+
+?>
