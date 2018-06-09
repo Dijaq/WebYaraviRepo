c1141e88260fe48277d20e7e052f53ba540b1a7c
djavierq <dijaq08@gmail.com>
2018-06-09 01:07:55 -0500

    Entidad Role

---
+++ app/Role.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace radioyaravi;
+
+use Illuminate\Database\Eloquent\Model;
+use app\User;
+
+class Role extends Model
+{
+    public $table = 'role';
+
+    public function user()
+    {
+    	return $this->hasMany(User::class, 'idRole');
+    }
+}
