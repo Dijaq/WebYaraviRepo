b69ff88db3d71c75af3be0b4994bbc92b683c774
djavierq <dijaq08@gmail.com>
2018-06-09 02:44:00 -0500

    Corregir Login

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/etiquetas';
 
     /**
      * Create a new controller instance.
