b4216495cb12621aee42b3ddb363de529c8b74f0
djavierq <dijaq08@gmail.com>
2018-06-09 03:38:35 -0500

    Corregido error Label

--- app/Http/Controllers/MainProgramacionController.php
+++ app/Http/Controllers/MainProgramacionController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Publicidad;
-use App\label;
+use App\Label;
 use Config;
 
 class MainProgramacionController extends Controller
