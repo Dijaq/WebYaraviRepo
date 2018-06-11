a243863898b4365b5668368d43ae79285064a35b
djavierq <dijaq08@gmail.com>
2018-06-10 23:21:10 -0500

    Correcion Meta Facebook V2

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,8 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:title" content={{$detailnew->title}} />
+    <meta property="og:image" content={{asset('storage/'.$moreNew->contentnews[0]->dir_image)}}/>
 
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
