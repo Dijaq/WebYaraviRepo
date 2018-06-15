6c04f10be1d2875ecee3d727444240529d1a8619
djavierq <dijaq08@gmail.com>
2018-06-15 01:17:52 -0500

    Compartir facebook

--- resources/views/main_news/home.blade.php
+++ resources/views/main_news/home.blade.php
@@ -8,7 +8,7 @@
 				<div class="picture left" style="width:100%; position: relative;" > 
 					<div class="nota_etiqueta">{{$new_principal->label->name}}</div>
 					<div class="redes_etiqueta">
-						<a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.pachamamaradio.org%2F&amp;src=sdkpreparse"" ><i style=" position: absolute; right: 5px; top: 5px;   text-align: center;">
+						<a target="_blank" href={{'https://www.facebook.com/sharer/sharer.php?u='.$urlServidor.'noticia/'.$new_principal->label->name.'/'.$new_principal->titleUrl.'&amp;src=sdkpreparse'}} ><i style=" position: absolute; right: 5px; top: 5px;   text-align: center;">
 							<img src="https://www.facebook.com/images/fb_icon_325x325.png" class="img-responsive" width="22px">
 						</i>
 						</a>
@@ -29,7 +29,7 @@
 				<div class="picture left" style="width:100%; position: relative;"> 
 					<div class="nota_etiqueta">{{$new_secundaria->label->name}}</div>
 					<div class="redes_etiqueta">
-						<a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.pachamamaradio.org%2F&amp;src=sdkpreparse"" ><i style=" position: absolute; right: 5px; top: 5px;   text-align: center;">
+						<a target="_blank" href={{'https://www.facebook.com/sharer/sharer.php?u='.$urlServidor.'noticia/'.$new_secundaria->label->name.'/'.$new_secundaria->titleUrl.'&amp;src=sdkpreparse'}} ><i style=" position: absolute; right: 5px; top: 5px;   text-align: center;">
 							<img src="https://www.facebook.com/images/fb_icon_325x325.png" class="img-responsive" width="22px">
 						</i>
 						</a>
