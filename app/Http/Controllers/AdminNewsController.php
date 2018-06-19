813f24f9db1978c007c30e657ffdfc41a3dcf10a
djavierq <dijaq08@gmail.com>
2018-06-19 00:40:03 -0500

    corregido nombre de imagen

--- app/Http/Controllers/AdminNewsController.php
+++ app/Http/Controllers/AdminNewsController.php
@@ -62,9 +62,19 @@ class AdminNewsController extends Controller
      */
     public function store(CreateNoticiasRequest $request)
     {
+        //Seccion de Guardar imagen
         $image       = $request->file('dir_image');
-        $filename    = $image->getClientOriginalName();
-        
+        $name = "";
+        $nameImage = $image->getClientOriginalName();
+        for($i = 0; $i<strlen($nameImage); $i++)
+        {
+            $vocal = ord(substr($nameImage, $i, 1));
+            if(!(($vocal >= 97 && $vocal<= 122)||($vocal >= 48 && $vocal <= 57) || $vocal == 46))
+                $name .= '-';
+            else
+                $name .= substr($nameImage, $i, 1);
+        }
+        $filename    = date("Ymd-His", strtotime(now())).'_'.$name;
         $image_resize = \Image::make($image->getRealPath());
         list($width, $height) = getimagesize($image);
 
@@ -86,6 +96,7 @@ class AdminNewsController extends Controller
 
         //$directorio = $request->file('dir_image')->store('public/publicity'); 
         $directorio = 'news/'.$filename; 
+        //Fin de seccion de imagen
 
 
         //Creacion de la Noticia
