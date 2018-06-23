07ddc9e8164d218ec592cdb02eae10d89e1acc61
djavierq <dijaq08@gmail.com>
2018-06-23 00:27:02 -0500

    Color NavbarYaravi

--- resources/views/navbarYaravi.blade.php
+++ resources/views/navbarYaravi.blade.php
@@ -1,4 +1,4 @@
-	<nav class="navbar navbar-expand-sm" role="navigation" style="background: #537CD5;">
+	<nav class="navbar navbar-expand-sm" role="navigation" style="background: #081377;">
 			<!--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navegacion_fmYaravi" aria-controls="navegacion_fmYaravi" aria-expanded="false" aria-label="Toggle navigation">
 			    <span class="navbar-toggler-icon"></span>
 			</button>-->
