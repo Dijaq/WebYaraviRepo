6cc836396ab5a3434738f3fcad01e6844469e7dd
djavierq <dijaq08@gmail.com>
2018-12-21 09:37:15 -0500

    .0 corregido

--- resources/views/layoutNew.blade.php
+++ resources/views/layoutNew.blade.php
@@ -16,7 +16,7 @@
     <meta name="twitter:card" content="summary_large_image">
 	<meta name="twitter:site" content="@RadioYaravi">
 	<meta name="twitter:creator" content="@RadioYaravi">
-	<meta name="twitter:domain" content="https://radioyaravi.org.pe">
+	<meta name="twitter:domain" content="https://www.radioyaravi.org.pe">
 	<meta name="twitter:title" content="{{$detailNavegador->title}}">
 	<meta name="twitter:description" content="{{$detailNavegador->summary}}">
 	<meta name="twitter:image" content={{asset('storage/'.$detailNavegador->dirImage)}}>
@@ -26,18 +26,12 @@
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
     <script src="{{asset('js/app.js')}}"></script>
-    <!--<script src="{{asset('js/dropzone.js')}}"></script>-->
-
-	<!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>    
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>-->
 
 	<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 	<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
 	<script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 
-	<!-- Global site tag (gtag.js) - Google Analytics -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-121070627-1"></script>
 	<script>
 	  window.dataLayer = window.dataLayer || [];
