e981f1d53f209a7faab25b9d4faa6ac0d47ae20e
djavierq <dijaq08@gmail.com>
2019-01-24 00:01:42 -0500

    embedsemantic

--- public/ckeditor/config.js
+++ public/ckeditor/config.js
@@ -29,7 +29,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
 	config.removeButtons = 'Underline,Subscript,Superscript';
-	config.extraPlugins = 'youtube,image2,embed,embedbase';
+	config.extraPlugins = 'youtube,image2,embedbase,embedsemantic';
 
 	config.youtube_width = '720';
 	config.youtube_height = '450';
