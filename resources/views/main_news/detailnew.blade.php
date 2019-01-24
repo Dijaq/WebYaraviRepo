bcea0d2ec65e215f354f83ae1e791426d2e5d87a
djavierq <dijaq08@gmail.com>
2019-01-23 21:57:01 -0500

    fb-comments order

--- resources/views/main_news/detailnew.blade.php
+++ resources/views/main_news/detailnew.blade.php
@@ -45,7 +45,7 @@
 					<h1 style="margin-top: 20px;">Coméntanos</h1>
 				</div>
 				<div class="col-md-12">
-					<div class="fb-comments" data-href="{{$urlServidorComentarios}}noticia/{{$detailnew->label->name}}/{{$detailnew->titleUrl}}"  data-width="100%"  data-numposts="5"></div>
+					<div class="fb-comments" data-href="{{$urlServidorComentarios}}noticia/{{$detailnew->label->name}}/{{$detailnew->titleUrl}}"  data-width="100%"  data-numposts="5"  data-order-by="reverse_time"></div>
 				</div>
 			</div>
 		</div>
