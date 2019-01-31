fe6bbe468cb4bf747567285e0295f0ce395bed5a
djavierq <dijaq08@gmail.com>
2019-01-30 23:11:20 -0500

    Programacion actualizada

--- resources/views/maininformation/programacion/index.blade.php
+++ resources/views/maininformation/programacion/index.blade.php
@@ -4,215 +4,190 @@
 
 	<div class="row">
 		<div class="col-md-12">
-			<h1>Programacion</h1>
+			<h1 style="font-weight: bold;">Programación</h1>
 
 			<table class="table table-bordered">
 		        <thead>
 		          <tr>
-		            <th width="11%"></th>
-		            <th>LUNES</th>
-		            <th>MARTES</th>
-		            <th>MIERCOLES</th>
-		            <th>JUEVES</th>
-		            <th>VIERNES</th>
-		            <th>SÁBADO</th>
-		            <th>DOMINGO</th>
+		            <th width="12%"></th>
+		            <th style="background: #FF4000; color: white;">LUNES</th>
+		            <th style="background: #FF4000; color: white;">MARTES</th>
+		            <th style="background: #FF4000; color: white;">MIERCOLES</th>
+		            <th style="background: #FF4000; color: white;">JUEVES</th>
+		            <th style="background: #FF4000; color: white;">VIERNES</th>
+		            <th style="background: #81BEF7">SÁBADO</th>
+		            <th style="background: #81BEF7">DOMINGO</th>
 		          </tr>
 		        </thead>
 		        <tbody>
-		        	<tr>  
-                		<td>05:00 - 05:30</td>
-                		<td colspan="5" rowspan="6"><h1>Yaraví informa</h1>
-                			Noticiero informativo de análisis y comentario respecto al contexto local, nacional e internacional.
+	        	<tr>  
+                		<td style="background: black; color: white;">05:00 - 05:30</td>
+                		<td colspan="5" rowspan="6" style="background: #81BEF7"><h1>Yaraví informa - Primera Edición</h1>
+                			<h4>con Hugo Condori y Marialuz Cruz </h4>       
                 		</td>
-                		<td></td>
-                		<td></td>
+                               <td colspan="1" rowspan="6" style="background: #81BEF7"><h1>Yaraví informa - Primera Edición</h1>
+                                        <h4>con Miguel Angel Avendaño y Alexander Cornejo</h4>       
+                                </td>
+                                <td colspan="1" rowspan="8" style="background: #81BEF7"><h1>Yaraví informa - Primera Edición</h1>
+                                        <h4>con Yolanda Ahumada y Hugo Condori </h4> 
                 	</tr>
                 	<tr>
-                		<td>05:30 - 06:00</td>  
-                		<td></td>
-                		<td></td>
+                		<td style="background: black; color: white;">05:30 - 06:00</td>  
+                		
                 	</tr>
                 	<tr>  
-                		<td>06:00 - 06:30</td>
-                		<td></td>
-                		<td></td>
+                		<td style="background: black; color: white;">06:00 - 06:30</td>                		
                 	</tr>
                 	<tr>
-                		<td>06:30 - 07:00</td>
-                		<td></td>
-                		<td></td>
+                		<td style="background: black; color: white;">06:30 - 07:00</td>               		
                 	</tr>
                 	<tr>  
-                		<td>07:00 - 07:30</td>
-                		<td></td>
-                		<td></td>
+                		<td style="background: black; color: white;">07:00 - 07:30</td>           		
+                		
                 	</tr>
                 	<tr>
-                		<td>07:30 - 08:00</td>
-                		<td></td>
-                		<td></td>
+                		<td style="background: black; color: white;">07:30 - 08:00</td>
                 	</tr>
                 	<tr>  
-                		<td>08:00 - 08:30</td>
-                		<td colspan="5" rowspan="4"><h1>El Poder De Elegir</h1>
-                			Programa  de  análisis y participación ciudadana - político
+                		<td style="background: black; color: white;">08:00 - 08:30</td>
+                		<td colspan="5" rowspan="4" style="background-color: #7CC8D2"><h1>Porder Ciudadano</h1>
+                			<h4>con Miguel Angel Avendaño y Yolanda Ahumada</h4>
                 		</td>
-                		<td rowspan="4"><h1>La Picantería</h1>
-                		Programa de debate análisis semanal sobre acontecimientos coyuntruales / vigentes.</td>
-                		<td></td>
+                		<td rowspan="4" style="background-color: #7CC8D2"><h1>La Picantería</h1>
+                		<h4>Andres Javier y Hector Tintaya</h4></td>
+                		
                 	</tr>
                 	<tr>
-                		<td>08:30 - 09:00</td>  
-                		<td></td>
+                		<td style="background: black; color: white;">08:30 - 09:00</td>  
                 	</tr>
                 	<tr>
-                		<td>09:00 - 09:30</td>
-                		<td rowspan="6"><h1>De Pueblo en Pueblo</h1>
-                			Programa musical informativo sobre el acontecer de la ciudad y de los pueblos jóvenes.
+                		<td style="background: black; color: white;">09:00 - 09:30</td>
+                                <td rowspan="4"><h1>Música y Noticias</h1></td>
+                	</tr>
+                	<tr>
+                		<td style="background: black; color: white;">09:30 - 10:00</td>
+                	</tr>
+                	<tr>  
+                		<td style="background: black; color: white;">10:00 - 10:30</td>
+                		<td colspan="5" rowspan="4"><h1>Mil Colores Nueva Temporada</h1>
+                			<h4>con Idalina Naira Tipo</h4>
                 		</td>
+                                <td rowspan="2"><h1>Música Variada</h1></td>
                 	</tr>
                 	<tr>
-                		<td>09:30 - 10:00</td>
+                		<td style="background: black; color: white;">10:30 - 11:00</td> 
+                               
+                	</tr>
+                	<tr>
+                		<td style="background: black; color: white;">11:00 - 11:30</td> 
+                		<td rowspan="2"><h1>Repiensa más allá de la prevención</h1></td>
+                                <td rowspan="2" style="background: #B0BD71"><h1>Sobre la Chambra la Piedra</h1></td>
                 	</tr>
                 	<tr>  
-                		<td>10:00 - 10:30</td>
-                		<td colspan="5" rowspan="4"><h1>Mil Colores</h1>
-                			Radiorevista musical informativa con despachos directos y entrevistas de interés y con consultorios y concursos
+                		<td style="background: black; color: white;">11:30 - 12:00</td>
+                	</tr>
+                	<tr>  
+                		<td style="background: black; color: white;">12:00 - 12:30</td>
+                		<td colspan="5" rowspan="4" style="background: #81BEF7"><h1>Yaraví informa - Segunda Edición</h1><h4>con Carla García y Hugo Condori</h4>
                 		</td>
+                		<td rowspan="2" style="background: #81BEF7"><h1>Yaraví Informa - Segunda Edición</h1>
+                                        <h4>con Alexander y Andrés Javier</h4></td>
+                		<td rowspan="2"><h1>Música</h1></td>
                 	</tr>
                 	<tr>
-                		<td>10:30 - 11:00</td> 
-                		<td></td>
+                		<td style="background: black; color: white;">12:30 - 13:00</td>  
+                	</tr>
+                	<tr>  
+                		<td style="background: black; color: white;">13:00 - 13:30</td>
+                		<td rowspan="2"><h1>Con Quitarra y con Cajón</h1>
+                                        <h4>con Derly Gutierrez</h4></td>
+                		<td rowspan="2"><h1>Con Guitarra y con Cajón</h1></td>
                 	</tr>
                 	<tr>
-                		<td>11:00 - 11:30</td> 
-                		<td></td>
+                		<td style="background: black; color: white;">13:30 - 14:00</td> 
                 	</tr>
                 	<tr>  
-                		<td>11:30 - 12:00</td>
-                	</tr>
-                	<tr>  
-                		<td>12:00 - 12:30</td>
-                		<td colspan="5" rowspan="4"><h1>Yaraví informa</h1>
-                			Noticiero informativo de análisis y comentario respecto al contexto local, nacional e internacional</td>
-                		<td></td>
-                		<td></td>
+                		<td style="background: black; color: white;">14:00 - 14:30</td>
+                		<td colspan="5" rowspan="3"><h1>Programa Deportivo</h1>
+                		<h4>Los Deportivos de Yaraví</h4></td>
+                		<td rowspan="2"><h1>Sábado Sensacional</h1></td>
+                		<td rowspan="8"><h1>Música y Deportes</h1></td>
                 	</tr>
                 	<tr>
-                		<td>12:30 - 13:00</td>  
-                		<td></td>
-                		<td></td>
+                		<td style="background: black; color: white;">14:30 - 15:00</td>  
                 	</tr>
                 	<tr>  
-                		<td>13:00 - 13:30</td>
-                		<td></td>
-                		<td></td>
-                	</tr>
-                	<tr>
-                		<td>13:30 - 14:00</td> 
-                		<td></td>
-                		<td></td>
-                	</tr>
-                	<tr>  
-                		<td>14:00 - 14:30</td>
-                		<td colspan="5" rowspan="3"><h1>Los Deportivos</h1>
-                		Programa deportivo con información, comentarios, reportes y entrevistas en vivo.</td>
-                		<td></td>
+                		<td style="background: black; color: white;">15:00 - 15:30</td>
+                		<td rowspan="4"><h1>La Voz del Volante</h1></td>
                 		<td></td>
                 	</tr>
                 	<tr>
-                		<td>14:30 - 15:00</td>  
-                		<td></td>
-                		<td></td>
+                		<td style="background: black; color: white;">15:30 - 16:00</td>  
+                		<td colspan="5" rowspan="2"><h1>La Voz del Volante</h1>         		
+                                </td>
                 	</tr>
                 	<tr>  
-                		<td>15:00 - 15:30</td>
-                		<td></td>
-                		<td></td>
+                		<td style="background: black; color: white;">16:00 - 16:30</td>
+                	</tr>
+                	<tr>  
+                		<td style="background: black; color: white;">16:30 - 17:00</td>
+                		<td colspan="5" rowspan="5" style="background: #81BEF7"><h1>Yaraví Informa - Edición Central</h1>
+                			<h4>Karol Macetas, Héctor Tintaya y Alexander Cornejo</h4>
+                		
+                	</tr>
+                        <tr>
+                                <td style="background: black; color: white;">17:00 - 17:30</td>  
+                                <td rowspan="2"><h1>Música Variada</h1></td>
+                                
+                        </tr>
+                	<tr>
+                		<td style="background: black; color: white;">17:30 - 18:00</td>  
+                	</tr>
+                	<tr>  
+                		<td style="background: black; color: white;">18:00 - 18:30</td>
+                		<td rowspan="4"><h1>Identidad</h1><h4>Derly Quispe</h4></td>
+                		<td rowspan="4"><h1>Música Juvenil</h1></td>
                 	</tr>
                 	<tr>
-                		<td>15:30 - 16:00</td>  
-                		<td colspan="5" rowspan="3"><h1>El Volante Protesta</h1>
-                		Programa dirigido a los taxistas.</td>
-                		<td></td>
-                		<td></td>
+                		<td style="background: black; color: white;">18:30 - 19:00</td>  
+                		
                 	</tr>
                 	<tr>  
-                		<td>16:00 - 16:30</td>
-                		<td></td>
-                		<td></td>
-                	</tr>
-                	<tr>
-                		<td>16:30 - 17:00</td>  
-                		<td></td>
-                		<td></td>
-                	</tr>
-                	<tr>  
-                		<td>17:00 - 17:30</td>
-                		<td colspan="5" rowspan="4"><h1>Yaraví informa</h1>
-                			Noticiero informativo de análisis y comentario respecto al contexto local, nacional e internacional
-                		<td></td>
-                		<td></td>
-                	</tr>
-                	<tr>
-                		<td>17:30 - 18:00</td>  
-                		<td></td>
-                		<td></td>
-                	</tr>
-                	<tr>  
-                		<td>18:00 - 18:30</td>
-                		<td></td>
-                		<td></td>
-                	</tr>
-                	<tr>
-                		<td>18:30 - 19:00</td>  
-                		<td></td>
-                		<td></td>
-                	</tr>
-                	<tr>  
-                		<td>19:00 - 19:30</td>
+                		<td style="background: black; color: white;">19:00 - 19:30</td>
                 		<td colspan="5" rowspan="2"><h1>Yaravi en tu Corazón</h1>
-                		Programa de Música Romantica.</td>
-                		<td></td>
-                		<td></td>
+                		<h4>Berly Gutierrez</h4></td>
+                		
                 	</tr>
                 	<tr>
-                		<td>19:30 - 20:00</td>  
-                		<td></td>
-                		<td></td>
+                		<td style="background: black; color: white;">19:30 - 20:00</td>  
+                		
                 	</tr>
                 	<tr>  
-                		<td>20:00 - 20:30</td>
-                		<td colspan="5" rowspan="3"><h1>Identidad</h1>Programa de música latinoamericana</td>
-                		<td></td>
-                		<td></td>
+                		<td style="background: black; color: white;">20:00 - 20:30</td>
+                		<td colspan="5" rowspan="3"><h1>Identidad</h1><h4>Derly Quispe</h4></td>
+                		<td rowspan="3"><h1>Enlazando Nuestros Cantos</h1>
+                                        <h4>Jaime Velazquez</h4></td>
+                		<td rowspan="6"><h1>Bloque Musical</h1></td>
                 	</tr>
                 	<tr>
-                		<td>20:30 - 21:00</td>  
-                		<td></td>
-                		<td></td>
+                		<td style="background: black; color: white;">20:30 - 21:00</td>  
                 	</tr>
                 	<tr>  
-                		<td>21:00 - 21:30</td>
-                		<td></td>
-                		<td></td>
+                		<td style="background: black; color: white;">21:00 - 21:30</td>
                 	</tr>
                 	<tr>
-                		<td>21:30 - 22:00</td>  
-                		<td  colspan="5" rowspan="3"><h1>Enlazando Nuestros Cantos</h1></td>
-                		<td></td>
-                		<td></td>
+                		<td style="background: black; color: white;">21:30 - 22:00</td>  
+                		<td  colspan="2" rowspan="3"><h3>Enlazando Nuestros Cantos</h3><h4>Jaime Velazquez</h4></td>
+                                <td  colspan="1" rowspan="3"><h3>La voz de la esperanza</h3></td>
+                                <td  colspan="2" rowspan="3"><h3>Enlazando Nuestros Cantos</h3>
+                                        <h4>Jaime Velasquez</h4></td>
+                		<td rowspan="3"><h1>Bloque Musical</h1></td>
                 	</tr>
                 	<tr>  
-                		<td>22:00 - 22:30</td>
-                		<td></td>
-                		<td></td>
+                		<td style="background: black; color: white;">22:00 - 22:30</td>
                 	</tr>
                 	<tr>
-                		<td>22:30 - 23:00</td>  
-                		<td></td>
-                		<td></td>
+                		<td style="background: black; color: white;">22:30 - 23:00</td>  
                 	</tr>
 
 		        </tbody>
