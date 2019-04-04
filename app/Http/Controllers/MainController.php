17ef1c8d3b704ee3f431aa5286a4ab81698f81db
djavierq <dijaq08@gmail.com>
2019-04-03 21:13:27 -0500

    return opciones

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -25,8 +25,8 @@ class MainController extends Controller
     public function home()
     {
         $encuesta = Encuesta::with('encuestaOpciones')->orderBy('created_at','desc')->get()->first();
-        $maxvalueEncuesta = EncuestaOpciones::where('idEncuesta', $encuesta->id)->max('value');
-        return EncuestaOpciones::where('idEncuesta', $encuesta->id)->get();
+        $maxvalueEncuesta = EncuestaOpciones::where('idEncuesta', $encuesta->id)->orderBy('value','desc')->max('value');
+        return $maxvalueEncuesta;
 
         $empresariales = Empresarial::orderBy('fechaPublicacion','desc')->where('estado', Config::get('constantes.estado_habilitado'))->take(Config::get('constantes.numero_empresariales'))->get();
 
