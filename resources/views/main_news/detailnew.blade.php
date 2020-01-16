24f4940ae0aedbb57c764127b4c290b1b5ec4e33
Dijaq <dijaq08@gmail.com>
2020-01-16 17:17:28 -0500

    separando lineas

--- resources/views/main_news/detailnew.blade.php
+++ resources/views/main_news/detailnew.blade.php
@@ -59,7 +59,7 @@
 					<img src="/storage/news/20200116-170401_vigilancia-electoral.jpeg" alt="Vigilancia Electoral" class="img-responsive" style="width:100%;"> 
 				</a>
 			</div>
-
+			<br>
 			
 			<div class="row" style="font-weight: bold;">
 				<div class="col-xs-12 col-sm-12 col-md-12">
