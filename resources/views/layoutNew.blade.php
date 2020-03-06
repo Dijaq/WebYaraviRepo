dd7dc77fe807c0c9a3efe8b125d9f13399d5b5ed
Dijaq <dijaq08@gmail.com>
2020-03-06 14:42:43 -0500

    new publicity politico

--- resources/views/layoutNew.blade.php
+++ resources/views/layoutNew.blade.php
@@ -197,7 +197,28 @@
 		    googletag.enableServices();
 		  });
 		</script>
-	
+	@elseif($detailnew->label->name == 'Político')
+		<!--ADS: Nota - politico-->
+
+		<script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"></script>
+
+		<script>
+		  window.googletag = window.googletag || {cmd: []};
+		  googletag.cmd.push(function() {
+		    googletag.defineSlot('/21907083702/yaravi/yaravi_rad_aqp_web_post_politico_top', [[970, 90], [970, 250], [300, 50], [300, 100], [728, 90], [320, 50], [1, 1], [320, 100]], 'gpt_top').addService(googletag.pubads());
+			  googletag.defineSlot('/21907083702/yaravi/yaravi_rad_aqp_web_post_politico_laterall', [[120, 600], [160, 600]], 'gpt_laterall').addService(googletag.pubads());
+		    googletag.defineSlot('/21907083702/yaravi/yaravi_rad_aqp_web_post_politico_lateralr', [[120, 600], [160, 600]], 'gpt_lateralr').addService(googletag.pubads());
+		    googletag.defineSlot('/21907083702/yaravi/yaravi_rad_aqp_web_post_politico_caja1', [[255, 255], [300, 250], [300, 100], [320, 100], [300, 50], [320, 50], [300, 600]], 'gpt_caja1').addService(googletag.pubads());
+		    googletag.defineSlot('/21907083702/yaravi/yaravi_rad_aqp_web_post_politico_caja2', [[300, 250], [300, 100], [320, 100], [300, 50], [320, 50], [300, 600]], 'gpt_caja2').addService(googletag.pubads());
+		    googletag.defineSlot('/21907083702/yaravi/yaravi_rad_aqp_web_post_politico_inline', [1, 1], 'gpt_inline').addService(googletag.pubads());
+		    googletag.defineSlot('/21907083702/yaravi/yaravi_rad_aqp_web_post_politico_vslider', [1, 1], 'gpt_vslider').addService(googletag.pubads());
+		    googletag.defineSlot('/21907083702/yaravi/yaravi_rad_aqp_web_post_politico_zocalo', [[728, 90], [320, 50]], 'gpt_zocalo').addService(googletag.pubads());
+		    googletag.pubads().enableSingleRequest();
+		    googletag.pubads().collapseEmptyDivs();
+		    googletag.pubads().setCentering(true);
+		    googletag.enableServices();
+		  });
+		</script>
 	@else
 		<!--No incluido-->
 	@endif
