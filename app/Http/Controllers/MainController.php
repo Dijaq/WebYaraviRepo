2f8d8b9ec6def416fe28787d6e9e714000457f5e
dijaq <dijaq08@gmail.com>
2020-10-26 22:56:10 -0500

    tipo go

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -283,8 +283,9 @@ class MainController extends Controller
 
         $listaUltimasNoticias = News::with('label')->with('contentnews')->where('estado', Config::get('constantes.estado_habilitado'))->orderBy('fechaPublicacion', 'desc')->take(Config::get('constantes.numero_ultimas_noticias'))->get();
         $urlServidor = Config::get('constantes.ruta_directorio');
+        $tipo_go_publicidad = 'HOME';
 
-        return view('main_news.classifiedCampanias', compact('publicidades', 'publicidadesPrincipal','campanias', 'new_principal', 'new_secundaria', 'labels', 'encuesta', 'tipoNoticia', 'listaUltimasNoticias', 'urlServidor'));
+        return view('main_news.classifiedCampanias', compact('publicidades', 'publicidadesPrincipal','campanias', 'new_principal', 'new_secundaria', 'labels', 'encuesta', 'tipoNoticia', 'listaUltimasNoticias', 'urlServidor', 'tipo_go_publicidad'));
     }
 
 }
