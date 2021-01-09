72809fed864e03f0b17f5a6f7a582501c6edb495
Diego Javier <dijaq08@gmail.com>
2021-01-08 22:36:13 -0500

    new zocalo

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -146,16 +146,15 @@
 								    googletag.cmd.push(function() { googletag.display('gpt_zocalo'); });
 								  </script>-->
 								  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-								<!-- yaravi_zocalo -->
-								<ins class="adsbygoogle"
-									style="display:block"
-									data-ad-client="ca-pub-8047009650931790"
-									data-ad-slot="9230918766"
-									data-ad-format="auto"
-									data-full-width-responsive="true"></ins>
-								<script>
-									(adsbygoogle = window.adsbygoogle || []).push({});
-								</script>
+									<ins class="adsbygoogle"
+										style="display:block"
+										data-ad-format="fluid"
+										data-ad-layout-key="-gw-3+1f-3d+2z"
+										data-ad-client="ca-pub-8047009650931790"
+										data-ad-slot="2487199401"></ins>
+									<script>
+										(adsbygoogle = window.adsbygoogle || []).push({});
+									</script>
 								</div>	
 							</div>
 						</div>
